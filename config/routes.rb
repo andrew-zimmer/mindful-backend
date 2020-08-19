36db337aac4dad7ac2536fc39849b03927d5b03f
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-08-19 14:48:05 -0400

    finished log in and log out functionality and the ability to create user with api request

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-#devise_for :users
+devise_for :users
 
-  namespace :v1 do
-    resources :sessions, only: %i[create destroy]
+  namespace :v1, defaults: { format: :json } do
+    resource :sessions, only: [:create, :destroy]
+    resources :users, only: [:create]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
